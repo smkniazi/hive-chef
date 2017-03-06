e05ded7dbed5c34e40ef8aa62635db68e72dd2b5
Jim Dowling <jdowling@sics.se>
2017-03-07 00:25:01 +0100

    install/dir fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,9 +8,9 @@ default.hive2.group                   = node.hops.group
 default.hive2.version                 = "2.2.0-SNAPSHOT"
 default.hive2.url                     = "#{node.download_url}/apache-hive-#{node.hive2.version}-bin.tar.gz"
 default.hive2.port                    = "2222"
-default.hive2.dir                     = "/srv"
-default.hive2.home                    =  node.hive2.dir + "/apache-hive-" + node.hive2.version + "-bin"
-default.hive2.base_dir                =  node.hive2.dir + "/apache-hive"
+default.hive2.dir                     = node.install.dir.empty? ? "/srv" : node.install.dir
+default.hive2.home                    = node.hive2.dir + "/apache-hive-" + node.hive2.version + "-bin"
+default.hive2.base_dir                = node.hive2.dir + "/apache-hive"
 default.hive2.keystore                = "#{node.kagent.base_dir}/node_server_keystore.jks"
 default.hive2.keystore_password       = "changeit"
 
