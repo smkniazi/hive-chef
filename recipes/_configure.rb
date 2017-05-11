bda9922ce9b8e594331d23e916027a1f9d66990e
Jim Dowling <jdowling@sics.se>
2017-05-11 10:20:28 +0200

    move cleaner install to _configure, as Karamel is not ordering install.rb recipes, and hops::install may not have run before hive::install

--- recipes/_configure.rb
+++ recipes/_configure.rb
@@ -9,6 +9,47 @@ zk_endpoints = zk_ips.join(",")
 mysql_endpoint = private_recipe_ip("ndb", "mysqld") + ":#{node.ndb.mysql_port}"
 
 
+
+# Download Hive cleaner
+package_url = "#{node.hive2.hive_cleaner.url}"
+base_package_filename = File.basename(package_url)
+cached_package_filename = "/tmp/#{base_package_filename}"
+
+remote_file cached_package_filename do
+  source package_url
+  owner node.hops.hdfs.user
+  group node.hops.group
+  mode "0644"
+  action :create_if_missing
+end
+
+cleaner_downloaded = "#{node.hive2.home}/.cleaner_extracted_#{node.hive2.hive_cleaner.version}"
+
+bash 'extract-cleaner' do
+        user node.hops.hdfs.user
+        group node.hops.group
+        code <<-EOH
+                set -e
+                tar zxf #{cached_package_filename} -C /tmp
+                mv /tmp/hivecleaner-#{node.hive2.hive_cleaner.version}/hive_cleaner #{node.hive2.base_dir}/bin/
+                touch #{cleaner_downloaded}
+        EOH
+     not_if { ::File.exists?( "#{cleaner_downloaded}" ) }
+end
+
+#Add the wiper
+file "#{node.hive2.base_dir}/bin/wiper.sh" do
+  action :delete
+end
+
+template "#{node.hive2.base_dir}/bin/wiper.sh" do
+  source "wiper.sh.erb"
+  owner node.hops.hdfs.user
+  group node.hops.group
+  mode 0755
+end
+
+
 endpoint = "http"
 if node["install"].attribute?("ssl") == true
   if node["install"]["ssl"] == "true"
