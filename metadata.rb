0c8259f2cfe8e9aa938918220de856ba788e9f2c
Fabio Buso <dev.siroibaf@gmail.com>
2017-05-23 14:41:34 +0200

    Set ordering for standalone tez recipe

--- metadata.rb
+++ metadata.rb
@@ -22,7 +22,7 @@ recipe           "install", "Installs a Hive2 Server"
 recipe           "default", "Starts both a Hive metastore and server2 and tez"
 recipe           "metastore", "Starts  a Hive Metastore Server"
 recipe           "server2", "Starts  a Hive Server2"
-recipe           "tez", "Starts  a Hive Server2"
+recipe           "tez", "Install Tez"
 recipe           "purge", "Removes and deletes an installed Hive Server"
 
 attribute "java/jdk_version",
