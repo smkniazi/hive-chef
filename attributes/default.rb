79086c42ba4ce98f5536bd45885ef94dc3a716b2
Fabio Buso <dev.siroibaf@gmail.com>
2017-10-16 21:10:36 +0200

    add attribute for scratchdir

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,6 +14,7 @@ default['hive2']['home']                    = node['hive2']['dir'] + "/apache-hi
 default['hive2']['base_dir']                = node['hive2']['dir'] + "/apache-hive"
 default['hive2']['logs_dir']                = node['hive2']['base_dir'] + "/logs"
 default['hive2']['hopsfs_dir']              = "/apps/hive"
+default['hive2']['scratch_dir']              = "/tmp/hive"
 default['hive2']['keystore']                = "#{node['kagent']['base_dir']}/node_server_keystore.jks"
 default['hive2']['keystore_password']       = "changeit"
 
